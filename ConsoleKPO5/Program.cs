fb2761156438e87a3733bc223deb599a11a54a3d
Isuly <Isulyshka@mail.ru>
2018-11-19 17:52:29 +0300

    program+метод sort

--- ConsoleKPO5/Program.cs
+++ ConsoleKPO5/Program.cs
@@ -10,10 +10,32 @@ namespace ConsoleKPO5
     {
         static void Main(string[] args)
         {
+            int size = 0;
+            int i = 0;
             //запрос размера массива
+            Console.WriteLine("Enter size:");
+            Int32.TryParse(Console.ReadLine(), out size);
+            int[] mass = new int[size];
+            int[] result = new int[size];
             //запрос ввода данных из консоли
+            Console.WriteLine("Enter numbers:");
             //считывание исходного массива чисел из консоли
+            while (i < size)
+            {
+                //tmp = Console.ReadLine()
+                Int32.TryParse(Console.ReadLine(), out mass[i]);
+                i++;
+            }
             //вызов метода сортировки и передать тудп исходный массив
+            HeapSort heapSort = new HeapSort();
+            result = heapSort.Sort(mass, size);
+            i = 0;
+            while (i < size)
+            {
+                Console.WriteLine(result[i].ToString());
+                i++;
+            }
+            Console.ReadKey(true);
         }
     }
 }
